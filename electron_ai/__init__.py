9a3828907c7c898853bb68091ad05f252068b6f1
IlarionDossouyovo <dossouyovoauguste@gmail.com>
2026-04-06 17:27:05 +0100

    Add electron_ai/__init__.py

---
+++ electron_ai/__init__.py
@@ -0,0 +1,11 @@
+"""
+ELECTRON AI - Système d'Intelligence Artificielle multi-agents
+pour ELECTRON - Web Design & Marketing Digital au Benin
+"""
+
+__version__ = "0.1.0"
+__author__ = "ELECTRON - Benin"
+
+from .main import main
+
+__all__ = ["main"]
